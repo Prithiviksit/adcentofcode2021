218604e1ff24879b8a48818e0543d31f697dc3f3
Coquelicot <yu.xb@outlook.com>
2021-12-15 12:34:47 -0500

    Update d2.R

--- d2.R
+++ d2.R
@@ -3,9 +3,24 @@ library(data.table)
 library(magrittr)
 library(zoo)
 
-dt<-fread(url("https://adventofcode.com/2021/day/2/input"))
-dt<-fread("https://adventofcode.com/2021/day/2/input")
+dt<-fread("input2.txt")
+dt[,sum(V2),by="V1"]
+1965*(2140-958)
 
-library(readr)
-input <- read_csv("https://adventofcode.com/2021/day/2/input")
-View(input)
+depth=0
+aim=0
+horizontal=0
+for (i in 1:nrow(dt)){
+  if(dt[i,V1]=="down"){
+    aim=aim+dt[i,V2]
+  }
+  if(dt[i,V1]=="up"){
+    aim=aim-dt[i,V2]
+  }
+  if(dt[i,V1]=="forward"){
+    horizontal=horizontal+dt[i,V2]
+    depth=depth+dt[i,V2]*aim
+  }
+}
+
+horizontal*depth
