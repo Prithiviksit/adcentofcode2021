010a1c2b99a027885c2cf9b98ff4846fa33da5f3
Coquelicot <yu.xb@outlook.com>
2021-12-15 12:33:12 -0500

    day 2

---
+++ d2.R
@@ -0,0 +1,11 @@
+library(rio)
+library(data.table)
+library(magrittr)
+library(zoo)
+
+dt<-fread(url("https://adventofcode.com/2021/day/2/input"))
+dt<-fread("https://adventofcode.com/2021/day/2/input")
+
+library(readr)
+input <- read_csv("https://adventofcode.com/2021/day/2/input")
+View(input)
