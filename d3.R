2427eebb12a133c41fc2567ba852cd214cd94376
Coquelicot <yu.xb@outlook.com>
2021-12-16 13:39:28 -0500

    Update d3.R

--- d3.R
+++ d3.R
@@ -28,23 +28,55 @@ apply(dt3,2,Add_char) %>% `<`(500) %>% as.numeric() %>% paste0(.,collapse = "") 
 most_common<-function(v){
   value<-ifelse(Add_char(v)>=length(v)/2,1,0)
   pos<-which(v==value)
-  return(list(value,pos))
+  #return(list(value,pos))
+  return(pos)
 }
 
 
-most_common(c("1","0","1","0","1","0"))
+most_common(c("0","1","1","1","0","0"))
 
+shrink<-function(l){
+  if(nrow(l$df)<=1){
+    return(l)
+  }else{
+    l$df[,l$i]%>% most_common -> keep
+    df<-l$df[keep,]
+    i=(l$i)%%12+1
+    print(c(length(keep),i))
+    return(shrink(list(df=df,i=i)))
+  }
+}
 
+l<-shrink(list(df=dt3,i=1))
+l$df%>% as.numeric() %>% paste0(.,collapse = "") %>% strtoi(.,base=2)
+#825
 
 ######### find CO2
 
 least_common<-function(v){
   value<-ifelse(Add_char(v)<length(v)/2,1,0)
   pos<-which(v==value)
-  return(list(value,pos))
+  #return(list(value,pos))
+  return(pos)
 }
 
 
 least_common(c("1","0","1","0","1","0"))
 
+shrink2<-function(l){
+  if(nrow(l$df)<=1){
+    return(l)
+  }else{
+    l$df[,l$i]%>% least_common -> keep
+    print(c(length(keep),l$i))
+    df<-l$df[keep,]
+    i=(l$i)%%12+1
+    print(c(length(keep),i))
+    return(shrink2(list(df=df,i=i)))
+  }
+}
 
+l<-shrink2(list(df=dt3,i=1))
+l$df%>% as.numeric() %>% paste0(.,collapse = "") %>% strtoi(.,base=2)
+#3375
+3375*825
